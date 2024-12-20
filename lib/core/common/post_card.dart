42d08b3c3204aa67f7ae4e99978d9b33d30af932
sachi-tmg <sachitamang43210@gmail.com>
2024-12-20 17:05:18 +0545

    changed the location

--- lib/core/common/post_card.dart
+++ lib/core/common/post_card.dart
@@ -32,7 +32,8 @@ class _PostCardState extends State<PostCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 5,
+      color: const Color.fromARGB(255, 250, 250, 250),
+      elevation: 2,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12),
       ),
