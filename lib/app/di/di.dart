38440308732d376a62d192dd4cd85c9fd55c8ee1
sachi-tmg <sachitamang43210@gmail.com>
2025-01-30 17:42:23 +0545

    linkora: remote dependncies added

--- lib/app/di/di.dart
+++ lib/app/di/di.dart
@@ -1,7 +1,12 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:linkora_app/app/shared_prefs/token_shared_prefs.dart';
+import 'package:linkora_app/core/network/api_service.dart';
 import 'package:linkora_app/core/network/hive_service.dart';
 import 'package:linkora_app/features/auth/data/data_source/local_datasource/auth_local_data_source.dart';
+import 'package:linkora_app/features/auth/data/data_source/remote_datasource/auth_remote_data_source.dart';
 import 'package:linkora_app/features/auth/data/repository/auth_local_repository/auth_local_repository.dart';
+import 'package:linkora_app/features/auth/data/repository/auth_remote_repository/auth_remote_repository.dart';
 import 'package:linkora_app/features/auth/domain/use_case/login_usecase.dart';
 import 'package:linkora_app/features/auth/domain/use_case/register_usecase.dart';
 import 'package:linkora_app/features/auth/presentation/view_model/login/login_bloc.dart';
@@ -9,11 +14,14 @@ import 'package:linkora_app/features/auth/presentation/view_model/signup/registe
 import 'package:linkora_app/features/home/presentation/view_model/home_cubit.dart';
 import 'package:linkora_app/features/onboarding/presentation/view_model/onboarding_bloc.dart';
 import 'package:linkora_app/features/splash/presentation/view_model/splash_cubit.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final getIt = GetIt.instance;
 
 Future<void> initDependencies() async {
   await _initHiveService();
+  await _initApiService();
+  await _initSharedPreferences();
   await _initSplashDependencies();
   await _initOnboardingDependencies();
   await _initHomeDependencies();
@@ -22,8 +30,65 @@ Future<void> initDependencies() async {
   await _initLoginDependencies();
 }
 
+Future<void> _initSharedPreferences() async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+  getIt.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+}
+
+_initApiService() {
+  getIt.registerLazySingleton<Dio>(
+    () => ApiService(Dio()).dio,
+  );
+}
+
 _initHiveService() {
   getIt.registerLazySingleton<HiveService>(() => HiveService());
+}
+
+_initRegisterDependencies() async {
+  // init local data source
+  getIt.registerLazySingleton(
+    () => AuthLocalDataSource(getIt<HiveService>()),
+  );
+
+  getIt.registerLazySingleton<AuthRemoteDataSource>(
+    () => AuthRemoteDataSource(getIt<Dio>()),
+  );
+
+  // init local repository
+  getIt.registerLazySingleton(
+    () => AuthLocalRepository(getIt<AuthLocalDataSource>()),
+  );
+
+  getIt.registerLazySingleton(
+    () => AuthRemoteRepository(getIt<AuthRemoteDataSource>()),
+  );
+
+  // register use usecase
+  // getIt.registerLazySingleton<RegisterUseCase>(
+  //   () => RegisterUseCase(
+  //     getIt<AuthLocalRepository>(),
+  //   ),
+  // );
+
+  getIt.registerLazySingleton<RegisterUseCase>(
+    () => RegisterUseCase(
+      getIt<AuthRemoteRepository>(),
+    ),
+  );
+
+  getIt.registerLazySingleton<UploadImageUsecase>(
+    () => UploadImageUsecase(
+      getIt<AuthRemoteRepository>(),
+    ),
+  );
+
+  getIt.registerFactory<RegisterBloc>(
+    () => RegisterBloc(
+      registerUseCase: getIt<RegisterUseCase>(),
+      uploadImageUsecase: getIt<UploadImageUsecase>(),
+    ),
+  );
 }
 
 _initSplashDependencies() {
@@ -44,31 +109,6 @@ _initDashboardDependencies() async {
   // );
 }
 
-_initRegisterDependencies() async {
-  // init local data source
-  getIt.registerLazySingleton(
-    () => AuthLocalDataSource(getIt<HiveService>()),
-  );
-
-  // init local repository
-  getIt.registerLazySingleton(
-    () => AuthLocalRepository(getIt<AuthLocalDataSource>()),
-  );
-
-  // register use usecase
-  getIt.registerLazySingleton<RegisterUseCase>(
-    () => RegisterUseCase(
-      getIt<AuthLocalRepository>(),
-    ),
-  );
-
-  getIt.registerFactory<RegisterBloc>(
-    () => RegisterBloc(
-      registerUseCase: getIt<RegisterUseCase>(),
-    ),
-  );
-}
-
 _initHomeDependencies() async {
   getIt.registerFactory<HomeCubit>(
     () => HomeCubit(),
@@ -76,9 +116,14 @@ _initHomeDependencies() async {
 }
 
 _initLoginDependencies() async {
+  getIt.registerLazySingleton<TokenSharedPrefs>(
+    () => TokenSharedPrefs(getIt<SharedPreferences>()),
+  );
+
   getIt.registerLazySingleton<LoginUseCase>(
     () => LoginUseCase(
-      getIt<AuthLocalRepository>(),
+      getIt<AuthRemoteRepository>(),
+      getIt<TokenSharedPrefs>(),
     ),
   );
 
