59772330de485bc59f47c4108dc410dfc37b6e00
sachi-tmg <sachitamang43210@gmail.com>
2025-01-30 17:39:04 +0545

    linkora: image part 2

--- lib/features/auth/domain/use_case/register_usecase.dart
+++ lib/features/auth/domain/use_case/register_usecase.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:linkora_app/app/usecase/usecase.dart';
@@ -9,21 +11,24 @@ class RegisterUserParams extends Equatable {
   final String fullName;
   final String email;
   final String password;
+  final String? image;
 
   const RegisterUserParams({
     required this.fullName,
     required this.email,
     required this.password,
+    this.image,
   });
 
   const RegisterUserParams.initial({
     required this.fullName,
     required this.email,
     required this.password,
+    this.image,
   });
 
   @override
-  List<Object?> get props => [fullName, email, password];
+  List<Object?> get props => [fullName, email, password, image];
 }
 
 class RegisterUseCase implements UsecaseWithParams<void, RegisterUserParams> {
@@ -37,7 +42,28 @@ class RegisterUseCase implements UsecaseWithParams<void, RegisterUserParams> {
       fullName: params.fullName,
       email: params.email,
       password: params.password,
+      image: params.image,
     );
     return repository.registerUser(authEntity);
+  }
+}
+
+class UploadImageParams {
+  final File file;
+
+  const UploadImageParams({
+    required this.file,
+  });
+}
+
+class UploadImageUsecase
+    implements UsecaseWithParams<String, UploadImageParams> {
+  final IAuthRepository _repository;
+
+  UploadImageUsecase(this._repository);
+
+  @override
+  Future<Either<Failure, String>> call(UploadImageParams params) {
+    return _repository.uploadProfilePicture(params.file);
   }
 }
