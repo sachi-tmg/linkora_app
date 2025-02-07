93f5d32d4c893c7a7a3f05afce10cffb2e8d6a87
sachi-tmg <sachitamang43210@gmail.com>
2025-02-07 11:13:16 +0545

    linkora: mock auth repo created

---
+++ test/features/auth/domain/use_case/repository.mock.dart
@@ -0,0 +1,4 @@
+import 'package:linkora_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthRepository extends Mock implements IAuthRepository {}
