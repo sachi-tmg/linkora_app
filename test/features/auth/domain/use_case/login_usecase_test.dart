18d06c31d02132905b46faf90fe172ff05ed0fb9
sachi-tmg <sachitamang43210@gmail.com>
2025-02-07 11:37:48 +0545

    linkora: grouping all login tests

--- test/features/auth/domain/use_case/login_usecase_test.dart
+++ test/features/auth/domain/use_case/login_usecase_test.dart
@@ -1,14 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:linkora_app/app/shared_prefs/token_shared_prefs.dart';
 import 'package:linkora_app/core/error/failure.dart';
-import 'package:linkora_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:linkora_app/features/auth/domain/use_case/login_usecase.dart';
 import 'package:mocktail/mocktail.dart';
 
-class MockAuthRepository extends Mock implements IAuthRepository {}
-
-class MockTokenSharedPrefs extends Mock implements TokenSharedPrefs {}
+import 'repository.mock.dart';
+import 'token.mock.dart';
 
 void main() {
   late MockAuthRepository mockAuthRepository;
@@ -25,91 +22,95 @@ void main() {
   const tPassword = 'sachi123';
   const tToken = 'abc1234';
 
-  test('should call login and save token successfully', () async {
-    // Arrange
-    when(() => mockAuthRepository.loginUser(tEmail, tPassword)).thenAnswer(
-      (_) async => const Right(tToken),
-    );
-    when(() => mockTokenSharedPrefs.saveToken(tToken)).thenAnswer(
-      (_) async => const Right(null),
-    );
-    when(() => mockTokenSharedPrefs.getToken()).thenAnswer(
-      (_) async => const Right(tToken),
-    );
+  group('Login Tests', () {
+    test('should call login and save token successfully', () async {
+      // Arrange
+      when(() => mockAuthRepository.loginUser(tEmail, tPassword)).thenAnswer(
+        (_) async => const Right(tToken),
+      );
+      when(() => mockTokenSharedPrefs.saveToken(tToken)).thenAnswer(
+        (_) async => const Right(null),
+      );
+      when(() => mockTokenSharedPrefs.getToken()).thenAnswer(
+        (_) async => const Right(tToken),
+      );
 
-    // Act
-    final result = await loginUseCase(
-        const LoginParams(email: tEmail, password: tPassword));
+      // Act
+      final result = await loginUseCase(
+          const LoginParams(email: tEmail, password: tPassword));
 
-    // Assert
-    expect(
-        result,
-        const Right(
-            tToken)); // Expect the result to be the token wrapped in Right
-    verify(() => mockAuthRepository.loginUser(tEmail, tPassword)).called(1);
-    verify(() => mockTokenSharedPrefs.saveToken(tToken)).called(1);
-    verify(() => mockTokenSharedPrefs.getToken()).called(1);
-  });
+      // Assert
+      expect(
+          result,
+          const Right(
+              tToken)); // Expect the result to be the token wrapped in Right
+      verify(() => mockAuthRepository.loginUser(tEmail, tPassword)).called(1);
+      verify(() => mockTokenSharedPrefs.saveToken(tToken)).called(1);
+      verify(() => mockTokenSharedPrefs.getToken()).called(1);
+    });
 
-  test('should return failure when login fails', () async {
-    // Arrange
-    when(() => mockAuthRepository.loginUser(tEmail, tPassword)).thenAnswer(
-      (_) async => const Left(ApiFailure(message: 'Invalid credentials')),
-    );
+    test('should return failure when login fails', () async {
+      // Arrange
+      when(() => mockAuthRepository.loginUser(tEmail, tPassword)).thenAnswer(
+        (_) async => const Left(ApiFailure(message: 'Invalid credentials')),
+      );
 
-    // Act
-    final result = await loginUseCase(
-        const LoginParams(email: tEmail, password: tPassword));
+      // Act
+      final result = await loginUseCase(
+          const LoginParams(email: tEmail, password: tPassword));
 
-    // Assert
-    expect(result, const Left(ApiFailure(message: 'Invalid credentials')));
-    verify(() => mockAuthRepository.loginUser(tEmail, tPassword)).called(1);
-    verifyZeroInteractions(mockTokenSharedPrefs);
-  });
+      // Assert
+      expect(result, const Left(ApiFailure(message: 'Invalid credentials')));
+      verify(() => mockAuthRepository.loginUser(tEmail, tPassword)).called(1);
+      verifyZeroInteractions(mockTokenSharedPrefs);
+    });
 
-  test('returns Failure when email is empty', () async {
-    // Arrange
-    const emptyEmailData = LoginParams(email: "", password: "kirtan123");
-    when(() => mockAuthRepository.loginUser(any(), any())).thenAnswer(
-        (_) async => const Left(ApiFailure(message: "email cannot be empty")));
+    test('returns Failure when email is empty', () async {
+      // Arrange
+      const emptyEmailData = LoginParams(email: "", password: "kirtan123");
+      when(() => mockAuthRepository.loginUser(any(), any())).thenAnswer(
+          (_) async =>
+              const Left(ApiFailure(message: "email cannot be empty")));
 
-    // Act
-    final result = await loginUseCase(emptyEmailData);
+      // Act
+      final result = await loginUseCase(emptyEmailData);
 
-    // Assert
-    expect(result, const Left(ApiFailure(message: "email cannot be empty")));
-    verify(() => mockAuthRepository.loginUser(any(), any())).called(1);
-    verifyNever(() => mockTokenSharedPrefs.saveToken(any()));
-  });
+      // Assert
+      expect(result, const Left(ApiFailure(message: "email cannot be empty")));
+      verify(() => mockAuthRepository.loginUser(any(), any())).called(1);
+      verifyNever(() => mockTokenSharedPrefs.saveToken(any()));
+    });
 
-  test('returns Failure when password is empty', () async {
-    // Arrange
-    const emptyPasswordData = LoginParams(email: "kirtan", password: "");
-    when(() => mockAuthRepository.loginUser(any(), any())).thenAnswer(
-        (_) async =>
-            const Left(ApiFailure(message: "Password cannot be empty")));
+    test('returns Failure when password is empty', () async {
+      // Arrange
+      const emptyPasswordData = LoginParams(email: "kirtan", password: "");
+      when(() => mockAuthRepository.loginUser(any(), any())).thenAnswer(
+          (_) async =>
+              const Left(ApiFailure(message: "Password cannot be empty")));
 
-    // Act
-    final result = await loginUseCase(emptyPasswordData);
+      // Act
+      final result = await loginUseCase(emptyPasswordData);
 
-    // Assert
-    expect(result, const Left(ApiFailure(message: "Password cannot be empty")));
-    verify(() => mockAuthRepository.loginUser(any(), any())).called(1);
-    verifyNever(() => mockTokenSharedPrefs.saveToken(any()));
-  });
+      // Assert
+      expect(
+          result, const Left(ApiFailure(message: "Password cannot be empty")));
+      verify(() => mockAuthRepository.loginUser(any(), any())).called(1);
+      verifyNever(() => mockTokenSharedPrefs.saveToken(any()));
+    });
 
-  test('returns Failure on server error', () async {
-    // Arrange
-    when(() => mockAuthRepository.loginUser(any(), any())).thenAnswer(
-        (_) async => const Left(ApiFailure(message: "Server error")));
+    test('returns Failure on server error', () async {
+      // Arrange
+      when(() => mockAuthRepository.loginUser(any(), any())).thenAnswer(
+          (_) async => const Left(ApiFailure(message: "Server error")));
 
-    // Act
-    final result = await loginUseCase(
-        const LoginParams(email: tEmail, password: tPassword));
+      // Act
+      final result = await loginUseCase(
+          const LoginParams(email: tEmail, password: tPassword));
 
-    // Assert
-    expect(result, const Left(ApiFailure(message: "Server error")));
-    verify(() => mockAuthRepository.loginUser(any(), any())).called(1);
-    verifyNever(() => mockTokenSharedPrefs.saveToken(any()));
+      // Assert
+      expect(result, const Left(ApiFailure(message: "Server error")));
+      verify(() => mockAuthRepository.loginUser(any(), any())).called(1);
+      verifyNever(() => mockTokenSharedPrefs.saveToken(any()));
+    });
   });
 }
