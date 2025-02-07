d98826e5b77c7f64fd236bfae5d78094ce2fc39c
sachi-tmg <sachitamang43210@gmail.com>
2025-02-07 11:45:38 +0545

    linkora: mock regular user repo created

---
+++ test/features/regular_user/domain/use_case/repository.mock.dart
@@ -0,0 +1,5 @@
+import 'package:linkora_app/features/regular_user/domain/respository/regular_user_repository.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockRegularUserRepository extends Mock
+    implements IRegularUserRepository {}
